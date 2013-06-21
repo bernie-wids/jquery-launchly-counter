40a54ca7746cd70b651e8ae8c79222d972165226
Bernard Agustino Widjanarko <bernard.widjanarko@gmail.com>
2013-06-21 10:55:51 +0700

    added hover effect

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ $(document).ready(function() {
 	var counter = $('#counter').launchlyCounter({
 		start: 0,
 		end: 100,
-		duration: 1000
+		duration: 5000
 	});
 	
-	counter.go(1);
-
+	$('#counter').hover(function(){
+		$(this).addClass('come-in');
+		counter.go(1);
+	},function(){
+		$(this).removeClass('come-in');
+		counter.go(-1);
+	});
+	
 });
